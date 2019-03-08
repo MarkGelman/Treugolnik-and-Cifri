b6b00b348a894a868aeda28804ae16eb370b4dd2
MarkGelman <47790935+MarkGelman@users.noreply.github.com>
2019-03-08 22:36:19 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treugolnik_Cifr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num = int.Parse(Console.ReadLine());
+            int mult = 0;
+            int i = 1;
+            while (i <= num)
+            {
+                mult += i;
+                if (i == num - 1)
+                {
+                    i++;
+                    mult *= 10;
+                    continue;
+                }
+                Console.WriteLine(mult);
+                mult *= 10;
+                i++;
+            }
+            mult /= 10;
+            while (mult != 1)
+            {
+                mult /= 10;
+                Console.WriteLine(mult);
+            }
+
+            /*for (int i=1; i<=num;i++)
+              {
+                    for (int j=1; j<=i; j++)
+                    {
+                        if (i == num - 1)
+                            break;
+                        if (j!=i-1)
+                            Console.Write(j);
+                        else
+                            Console.WriteLine(j);
+                    }
+              }
+            for (int i = num-1; i>0;i--)
+             {
+                for (int j = 1; j <= i; j++)
+                {
+                    if (j != i - 1)
+                        Console.Write(j);
+                    else
+                        Console.WriteLine(j);
+                }
+            }*/
+        }
+    }
+}
